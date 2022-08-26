aaaa06646880eb34b3b6fdb59f602eb59f2598bc
JoeTeletubby <110197993+JoeTeletubby@users.noreply.github.com>
2022-08-26 17:54:07 +0200

    1.3.0
    added:
    - import / export
    - a brand new fancy error
    - full clear (except title and type lmao)

--- tables.py
+++ tables.py
@@ -1,16 +1,21 @@
 import texttable
 
-version = "1.2.0"
+version = "1.3.0"
 
-class MultipleInstancesError(Exception):
-    pass
+class MultipleInstancesError(Exception): pass
+class ImportInstancesAmountError(Exception): pass
+
+# Types:
+# 0: Standard with prettyness
+# 1: Without lines
 
 class Table:
-    def __init__(self, title, columns, headers):
+    def __init__(self, title, columns, headers, type_=0):
         self.title = title
         self.col_a = columns
         self.heads = headers
         self.table = []
+        self.type = type_
     
     def insert(self, values):
         toAppend = {}
@@ -52,10 +57,46 @@ class Table:
 
     def __str__(self):
         table = texttable.Texttable()
+        if self.type:
+            table.set_deco(texttable.Texttable.HEADER)
         table.header(self.heads)
         for x in self.table:
             table.add_row(x.values())
         return table.draw()
     
+    def clear(self):
+        self.table = []
+
+    def xclear(self):
+        self.col_a = 0
+        self.heads = []
+        self.table = []
+
+    def export(self):
+        string = ""
+        string += str(self.col_a) + ";"
+        string += ";".join(self.heads) + ";"
+        for x in self.table:
+            string += ";".join(list(map(str, x.values()))) + ";"
+        return string[:-1]
+
+    exportt = export
+
+    def importt(self, string):
+        values = string.split(";")
+        self.xclear()
+        self.col_a = int(values[0])
+        for x in range(0, self.col_a):
+            self.heads.append(values[x+1])
+        for x in range(0, self.col_a+1):
+            values.pop(0)
+        if len(values) % self.col_a != 0:
+            raise ImportInstancesAmountError
+        for x in range(0, int(len(values) / self.col_a)):
+            append = []
+            for y in range(0, self.col_a):
+                append.append(values[self.col_a * x + y])
+            self.insert(append)
+
     def __repr__(self):
         return f"<Table: {self.title} with values {self.table}>"
